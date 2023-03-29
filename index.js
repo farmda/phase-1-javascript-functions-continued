4f1362c0b30c5b07ca7a5f0bc009fd1bc8800675
David <davidafarmer@gmail.com>
2023-03-29 17:46:14 -0400

    This was harder then I thought

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+return(`This Saturday, I want to ${activity}!`);
+
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+
+
+
+
+
+mondayWork()
+mondayWork("This Monday, I will work from home")
+
+function wrapAdjective(highlight = "||"){
+    return function(adj = "special"){
+      return `You are ${highlight}${adj}${highlight}!`;
+    }
+  }
+
+//function wrapAdjective(discribe = "special"){
+   // return(`You are ${discribe}`);
+
+//}
+wrapAdjective();
